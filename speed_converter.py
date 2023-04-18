f277b9c2695d8864a6d4245036520f76adc43b57
lancercarstars991 <129785266+lancercarstars991@users.noreply.github.com>
2023-04-18 18:59:51 +0300

    Create speed_converter.py

---
+++ speed_converter.py
@@ -0,0 +1,7 @@
+def mph_to_kph(mph):
+    """Convert miles per hour to kilometers per hour."""
+    return mph * 1.609344
+
+def kph_to_mph(kph):
+    """Convert kilometers per hour to miles per hour."""
+    return kph / 1.609344
